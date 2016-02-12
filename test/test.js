c1d3fcac0e6b824f5ffd72ad3c48b5db804c2e3c
Blake Netzeband <blake.netzeband@gmail.com>
2016-02-12 10:14:50 -0700

    restructed testing statements. added divisible by 100 test

--- test/test.js
+++ test/test.js
@@ -2,25 +2,21 @@ var code = require('../main.js')
 var expect = require('chai').expect
 
 describe('leapyear(400)', function(){
-  it("return true - leap years should be divisable by 400", function(){
+  it("leap years should be divisable by 400", function(){
     expect(code.leapYear(400)).to.equal(true);
-  });
-});
-
-describe('leapyear(400)', function(){
-  it("return false - leap years should be divisable by 400", function(){
-    expect(code.leapYear(400)).to.equal(false);
+    expect(code.leapYear(401)).to.equal(false);
   });
 });
 
 describe('leapyear(4)', function(){
-  it("return true - leap years should be divisable by 4", function(){
+  it("leap years should be divisable by 4", function(){
     expect(code.leapYear(4)).to.equal(true);
+    expect(code.leapYear(3)).to.equal(false);
   });
 });
 
-describe('leapyear(4)', function(){
-  it("return false - leap years should be divisable by 4", function(){
-    expect(code.leapYear(4)).to.equal(false);
+describe('leapyear(100)', function(){
+  it("leap years should not be divisable by 100", function(){
+    expect(code.leapYear(100)).to.equal(false);
   });
 });
