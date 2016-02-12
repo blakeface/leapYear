fef9265195013340c60eaa578e9210b73b1780c3
Blake Netzeband <blake.netzeband@gmail.com>
2016-02-12 10:05:33 -0700

    tested 400 parameter

--- test/test.js
+++ test/test.js
@@ -2,7 +2,13 @@ var code = require('../main.js')
 var expect = require('chai').expect
 
 describe('leapyear(400)', function(){
-  it("it should say 'true'", function(){
+  it("return true - leap years should be divisable by 400", function(){
     expect(code.leapYear(400)).to.equal(true);
+  });
+});
+
+describe('leapyear(400)', function(){
+  it("return false - leap years should be divisable by 400", function(){
+    expect(code.leapYear(400)).to.equal(false);
   });
 });
