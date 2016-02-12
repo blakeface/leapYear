e470078a62d0e99b800cec858e582fcb45d4a736
Blake Netzeband <blake.netzeband@gmail.com>
2016-02-12 10:07:33 -0700

    tested 4 parameter

--- test/test.js
+++ test/test.js
@@ -12,3 +12,15 @@ describe('leapyear(400)', function(){
     expect(code.leapYear(400)).to.equal(false);
   });
 });
+
+describe('leapyear(4)', function(){
+  it("return true - leap years should be divisable by 4", function(){
+    expect(code.leapYear(4)).to.equal(true);
+  });
+});
+
+describe('leapyear(4)', function(){
+  it("return false - leap years should be divisable by 4", function(){
+    expect(code.leapYear(4)).to.equal(false);
+  });
+});
