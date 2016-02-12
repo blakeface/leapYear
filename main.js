5fea6613f2738c0dd411f523238c3310262ed520
Blake Netzeband <blake.netzeband@gmail.com>
2016-02-12 10:02:23 -0700

    corrected test.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-function leapYear (year) {
-  if ( year % 100 === 0 && year % 400 !== 0) return false;
-  if ( year % 4 === 0 ) return true;
-  return false;
+module.exports = {
+  leapYear: function (year) {
+    if ( year % 100 === 0 && year % 400 !== 0) return false;
+    if ( year % 4 === 0 ) return true;
+    return false;
+  }
 }
--- test/test.js
+++ test/test.js
@@ -1,5 +1,8 @@
-describe("leapyear", function(){
+var code = require('../main.js')
+var expect = require('chai').expect
+
+describe('leapyear(400)', function(){
   it("it should say 'true'", function(){
-    expect(code.leapyear()).to.equal(4);
+    expect(code.leapYear(400)).to.equal(true);
   });
 });
